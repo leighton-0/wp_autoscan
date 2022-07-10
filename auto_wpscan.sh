2d4830532a767244e8c7eb080e89dcecd928bff7
Leighton Clucas <63763567+leighton-0@users.noreply.github.com>
2022-07-11 04:06:29 +0800

    Update auto_wpscan.sh

--- auto_wpscan.sh
+++ auto_wpscan.sh
@@ -1,11 +1,13 @@
 wpscan --update
-read -p $'\e[1;92m Liste mit Wordpresswebistes: \e[0m' wl_pass 
+read -p $'\e[1;92m List of WordPress sites: \e[0m' wl_pass  # -p flag > prompt
 wl_pass="${wl_pass}"
 
-count_pass=$(wc -l $wl_pass | cut -d " " -f1)
+count_pass=$(wc -l $wl_pass | cut -d " " -f1)               #  wordcount lines piped to cut delimitor of space & field of 1st column
 
 for fn in `cat $wl_pass`; do
-wpscan --url $fn -e  u,vp  >> result
+wpscan --url $fn -e  u,vp  >> result                         # for the site named $fn -e (numerate U (USERS VP vulnerable plugins
+
+
 done
 
 print "Done, please check result"
