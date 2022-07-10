e9548bae7084d068fc66572ab3bfda1c65c4fa73
Leighton Clucas <63763567+leighton-0@users.noreply.github.com>
2022-07-11 04:51:24 +0800

    Update auto_wpscan.sh

--- auto_wpscan.sh
+++ auto_wpscan.sh
@@ -5,9 +5,20 @@ wl_pass="${wl_pass}"
 count_pass=$(wc -l $wl_pass | cut -d " " -f1)               #  wordcount lines piped to cut delimitor of space & field of 1st column
 
 for fn in `cat $wl_pass`; do
+
+: '
+  echo "+------------------------------------------+"
+  printf "| %-40s |\n" "`$fn`"
+  echo "|                                          |"
+  printf "|`tput bold` %-40s `tput sgr0`|\n" "$@"
+  echo "+------------------------------------------+"
+  '
+
 wpscan --url $fn -e  u,vp  >> result                         # for the site named $fn -e (numerate U (USERS VP vulnerable plugins
 
 
 done
 
 print "Done, please check result"
+
+
